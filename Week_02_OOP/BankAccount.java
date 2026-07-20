5c56058967f49a71cac8a6f55b4b3ec8ce0a7c5f
aaryapatkarworks <aaryapatkarworks@gmail.com>
2026-07-20 16:30:44 +0530

    Updated with Encapsulation Practice

--- Week_02_OOP/BankAccount.java
+++ Week_02_OOP/BankAccount.java
@@ -1,43 +1,75 @@
 public class BankAccount {
 
-    String accountHolder;
-    long accountNumber;
-    double balance;
+    private String accountHolder;
+    private long accountNumber;
+    private double balance;
 
+    // Default Constructor
     BankAccount() {
         accountHolder = "Unknown";
         accountNumber = 0;
         balance = 1000;
     }
 
-    //parameterized constructor
+    // Parameterized Constructor
     BankAccount(String accountHolder, long accountNumber, double balance) {
         this.accountHolder = accountHolder;
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
+    // Deposit Method
     void deposit(double amount) {
         balance = balance + amount;
         System.out.println("₹" + amount + " deposited successfully.");
     }
 
-    //method
+    //getters
+    String getAccountHolder() {
+        return accountHolder;
+    }
+
+    long getAccountNumber() {
+        return accountNumber;
+    }
+
+    double getBalance() {
+        return balance;
+    }
+
+    //setters
+    void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
     void displayAccount() {
-        System.out.println("Account Holder : " + accountHolder);
-        System.out.println("Account Number : " + accountNumber);
-        System.out.println("Balance        : ₹" + balance);
+        System.out.println("Account Holder : " + getAccountHolder());
+        System.out.println("Account Number : " + getAccountNumber());
+        System.out.println("Balance        : ₹" + getBalance());
         System.out.println();
     }
 
     public static void main(String[] args) {
 
         BankAccount account1 = new BankAccount();
-        BankAccount account2 = new BankAccount("Aarya Patkar", 123456789012L, 90000);
+
+        BankAccount account2 =
+                new BankAccount("Aarya Patkar", 123456789012L, 90000);
 
         account1.displayAccount();
+
         account2.displayAccount();
+
         account2.deposit(5000);
+
         account2.displayAccount();
+
+        // Change Account Holder
+
+        account2.setAccountHolder("Pahel Patkar");
+
+        System.out.println("Updated Name : "
+                + account2.getAccountHolder());
+
     }
 }
