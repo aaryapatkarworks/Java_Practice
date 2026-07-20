2698df6321774553c362f743a3a8ae6d9c5d857c
aaryapatkarworks <aaryapatkarworks@gmail.com>
2026-07-20 12:16:16 +0530

    Created New Directory for OOP; Added Class and Object basic code

---
+++ Week_02_OOP/_01_Class_Object.java
@@ -0,0 +1,35 @@
+public class _01_Class_Object {
+
+    String name;
+    byte age;
+    String branch;
+
+    void displayInfo() {
+        System.out.println("Name   : " + name);
+        System.out.println("Age    : " + age);
+        System.out.println("Branch : " + branch);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        _01_Class_Object s1 = new _01_Class_Object();
+        s1.name = "Aarya";
+        s1.age = 21;
+        s1.branch = "CSE AI-DS";
+
+        _01_Class_Object s2 = new _01_Class_Object();
+        s2.name = "Pahel";
+        s2.age = 20;
+        s2.branch = "CSE CSF";
+
+        _01_Class_Object s3 = new _01_Class_Object();
+        s3.name = "Shelly";
+        s3.age = 21;
+        s3.branch = "CSE Core";
+
+        s1.displayInfo();
+        s2.displayInfo();
+        s3.displayInfo();
+    }
+}
